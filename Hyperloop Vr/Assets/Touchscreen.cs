066abe202ed4605187687fda8cf4c98200fa5524
RoyalRB <Richardbenthem1423@gmail.com>
2021-03-08 12:18:58 +0100

    Touchscreen implementation

---
+++ Hyperloop Vr/Assets/Touchscreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touchscreen : MonoBehaviour
+{
+    [SerializeField] MeshRenderer disco;
+    [SerializeField] Color[] colors;
+    int index;
+    bool discoTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("IndexFinger"))
+        {
+            if (!discoTime)
+            {
+                discoTime = true;
+                StartCoroutine(Disco());
+            }
+            else
+            {
+                discoTime = false;
+            }
+        }
+            
+
+    }
+
+    IEnumerator Disco()
+    {
+        while (discoTime)
+        {
+            disco.material.color = colors[index];
+            index++;
+            if (index > 5)
+                index = 0;
+            yield return new WaitForSeconds(0.5f);
+            yield return null;
+        }
+        
+    }
+}
