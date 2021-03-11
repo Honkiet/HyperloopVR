e1970db5f31ec29b32ab67d19aecaab9bc9aec60
Hendrik Truong <truheco@gmail.com>
2021-03-11 20:34:13 +0100

    Added Faucet Functionality

---
+++ Hyperloop Vr/Assets/Scripts/FaucetWater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaucetWater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ShowWater()
+    {
+        transform.GetComponent<Renderer>().enabled = true;
+    }
+
+    public void StopWater()
+    {
+        transform.GetComponent<Renderer>().enabled = false;
+    }
+}
