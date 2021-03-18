6f64f94ba8863b1ce45cf12f7d2e27c5ab9ca502
Hendrik Truong <truheco@gmail.com>
2021-03-18 12:31:23 +0100

    Finished the Cans

--- Hyperloop Vr/Assets/Scripts/Sodacan.cs
+++ Hyperloop Vr/Assets/Scripts/Sodacan.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Sodacan : MonoBehaviour
 {
+
+    public ParticleSystem waterEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class Sodacan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Dot(transform.up, Vector3.down) > 0.2)
+        {
+            if (!waterEffect.isPlaying)
+            {
+                waterEffect.Play();
+            }
+            
+        }
+        else
+        {
+            
+            waterEffect.Stop();
+            
+        }
     }
 }
