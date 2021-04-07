393c7ab731272ef55700f6af93172dea766f9976
RoyalRB <Richardbenthem1423@gmail.com>
2021-04-07 21:20:19 +0200

    Finalize prototype

---
+++ Hyperloop Vr/Assets/EnterHyperloop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterHyperloop : MonoBehaviour
+{
+    [SerializeField] GameObject doorToOpen;
+    [SerializeField] Vector3 teleportDestination;
+    [SerializeField] Vector3 teleportRotation;
+    int frameEntered;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            doorToOpen.GetComponent<Animator>().Play("HyperloopDoorOpen");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(frameEntered != Time.frameCount)
+        {
+            doorToOpen.GetComponent<Animator>().Play("HyperloopDoorOpen");
+            GameObject.FindGameObjectWithTag("Player").GetComponent<CustomTeleport>().TeleportPlayer(teleportDestination, teleportRotation, 2f);
+        }
+    }
+}
---
+++ Hyperloop Vr/Assets/PhoneSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject phone;
+    [Header("Leave empty to spawn in front of player")]
+    [SerializeField] Vector3 spawnLocation;
+
+    public void SpawnPhone()
+    {
+        if (spawnLocation == null || spawnLocation == Vector3.zero)
+        {
+            phone.GetComponent<Phone>().SpawnInFrontOfPlayer();
+        }
+        else
+        {
+            phone.GetComponent<Phone>().SpawnAtLocation(spawnLocation);
+        }
+    }   
+}
