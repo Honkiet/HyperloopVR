2ab8f45f93d0a4f67e4d3dd622d3e17e4bda6185
Hendrik Truong <truheco@gmail.com>
2021-04-25 13:26:06 +0200

    Added animations to the seat. Todo make new buttons

---
+++ Hyperloop Vr/Assets/Scripts/SeatButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatButton : MonoBehaviour
+{
+    [SerializeField] GameObject hyperSeat;
+    int framePressed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("IndexFinger") && framePressed != Time.frameCount)
+        {
+            framePressed = Time.frameCount;
+            hyperSeat.GetComponent<Animator>().SetTrigger("down");
+        }
+    }
+}
